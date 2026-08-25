0be505b86d10ecc5882338a0b051a823dfbf697d
Alejandro-Palacios <palaciossalee@gmail.com>
2026-08-25 14:19:55 -0300

    se creó la base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,22 @@
 console.log("hola mundo");
+
+import { Sequelize } from "sequelize";
+
+// conexion a la base de datos
+
+export const sequelize = new Sequelize("Bd_alquileres", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+// testear la conexion
+
+export const startDB = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ force: true });
+    console.log("Conexion a la db esta lista");
+  } catch (error) {
+    console.error("No se pudo conectar a la db:", error);
+  }
+};
